314c2ec432b09472c85ba2f7b6aa2bdeed6546bf
Vinay V <cool00geek@yahoo.com>
2018-02-24 14:48:02 -0800

    Update postReboot.sh

--- Configs/postReboot.sh
+++ Configs/postReboot.sh
@@ -10,6 +10,6 @@ fingerprint-gui
 # Rclone
 echo "Copy the contents of this file to"
 echo "     ~/.config/rclone/rclone.conf"
-echo "     https://gist.github.com/billwi/7b3982fcd0be1b1bb1199fc1732e80fc"
+echo "     https://gist.github.com/cool00geek/7b3982fcd0be1b1bb1199fc1732e80fc"
 # Steam
 steam
